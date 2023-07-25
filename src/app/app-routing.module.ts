bcd128c5126646bf8bfde6be2a347f4eea3ec76d
anappi <you@example.com>
2023-07-25 16:26:26 +0200

    primo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PartyComponent } from './pages/party/party.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'party'
+  },
+  { path: 'party',
+  // canActivate:[AuthGuard],
+  component: PartyComponent,
+  data: {
+    title:'nessuno',
+  }
+},
+{path: 'party',
+  loadChildren: () =>
+    import('./pages/party/party.module').then((m) => m.PartyModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
+
 export class AppRoutingModule { }
